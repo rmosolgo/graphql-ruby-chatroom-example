9d3a50a9aa875fec4d414981285838052eb43bff
Robert Mosolgo <rdmosolgo@gmail.com>
2018-04-12 21:44:32 -0400

    Add todo

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 
-  mount ChatSchema.pusher_webhooks_client, at: "/pusher_webhooks"
+  # TODO: authenticate this endpoint!
   mount ChatSchema.dashboard, at: "/graphql/dashboard"
+
+  mount ChatSchema.pusher_webhooks_client, at: "/pusher_webhooks"
   mount ChatSchema.operation_store_sync, at: "/graphql/sync"
 end
